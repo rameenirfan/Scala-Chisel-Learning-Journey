88a421e0c71a4eb55f6fcddd120ad4952f070394
Rameen Irfan <rameenmuhammadirfan@gmail.com>
2021-11-07 07:57:12 -0800

    Add files via upload

---
+++ src/main/scala/gcd/logicgates/OR.scala
@@ -0,0 +1,10 @@
+package practice
+import chisel3._
+class OR extends Module {
+    val io = IO(new Bundle {
+        val a = Input(UInt(32.W))
+        val b = Input(UInt(32.W))
+        val output = Output(UInt(32.W))
+    })
+    io.output := io.a | io.b
+}
---
+++ src/main/scala/gcd/logicgates/XNOR.scala
@@ -0,0 +1,11 @@
+package practice
+import chisel3._
+class XNOR extends Module {
+    val io = IO(new Bundle {
+        val a = Input(UInt(32.W))
+        val b = Input(UInt(32.W))
+        val output = Output(UInt(32.W))
+    })
+    io.output := !(io.a ^ io.b)
+    
+}
