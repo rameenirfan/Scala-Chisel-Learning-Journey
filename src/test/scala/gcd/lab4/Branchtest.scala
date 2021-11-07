eb8a4d88b1b62487b0145174f19314b8804a8376
Rameen Irfan <rameenmuhammadirfan@gmail.com>
2021-11-07 08:08:06 -0800

    Add files via upload

---
+++ src/test/scala/gcd/lab4/Branchtest.scala
@@ -0,0 +1,46 @@
+package lab4
+import chisel3 . _
+import chisel3 . util
+import org . scalatest . _
+import chiseltest . _
+import chiseltest . experimental . TestOptionBuilder . _
+import chiseltest . internal . VerilatorBackendAnnotation
+import scala . util . Random
+import BALUOP._
+
+class Branchtest extends FreeSpec with ChiselScalatestTester {
+    "ALUTest" in {
+        test ( new Branch ) { c =>
+        // ALU operations
+            val array_op = Array ( ALU_BEQ,ALU_BNE,ALU_BGE,ALU_BLT )
+            for ( i <- 0 until 100) {
+                // println(s"i=>$i")
+                val src_a = Random.nextLong()&0xFFFFFFFFL
+                val src_b = Random.nextLong()&0xFFFFFFFFL
+                val opr = Random.nextInt(4)
+                val aluop = array_op ( opr )
+                //println(s"aluop=>$aluop")
+                // println(s"srcA=>$src_a*")
+                
+                // println(s"srcB=>$src_b")
+                val result:Int = aluop match {
+                    case ALU_BEQ => if(src_a.toInt == src_b.toInt) 1 else 0
+                    
+                    case ALU_BNE =>if(src_a.toInt != src_b.toInt) 1 else 0
+                    case ALU_BGE => if(src_a.toInt >= src_b.toInt) 1 else  0
+                    case ALU_BLT => if(src_a.toInt < src_b.toInt) 1 else  0
+                    // case _=> 0
+                }
+            
+            
+                    println(s"aluop=>$result")
+                    c.io.in_1.poke(src_a.S)
+                    c.io.in_2.poke(src_b.S)
+                    c.io.select.poke(aluop)
+                    c . clock . step (1)
+                    c . io . output.expect (result.B)
+                }
+            
+            c . clock . step (200)}
+        }
+    }
---
+++ src/test/scala/gcd/lab4/Immediatetest.scala
@@ -0,0 +1,37 @@
+package lab4
+import chisel3._
+import chisel3.util
+import org.scalatest._
+import chiseltest._
+import scala.util.Random
+
+class Immediatetest extends FreeSpec with ChiselScalatestTester {
+    "Imm  Test" in {
+        test(new Immediate){c =>
+
+            //val array_op = Array (53478035,5251619,239)
+            val array_op = Array(69271699,318575159,12583151)
+            for ( i <- 0 until 3) {
+                println (i)
+                val opr = Random.nextInt(2)
+                val src_ins = array_op(opr)
+                val result = src_ins match {
+                    case 69271699 => 66
+                    case 318575159 => 77777
+                    case 12583151 =>12
+                    // case 53478035 => 51
+                    // case 5251619=> 4
+                    // case 239 => 0 
+                    case _=> 0
+                }
+
+                c.io.ins.poke(src_ins.U)
+                c.io.pc.poke(0.U)
+                c.clock.step(1)
+                c.io.out.expect(result.S )
+            }
+            // c . clock . step (200)
+        }
+}
+}
+
